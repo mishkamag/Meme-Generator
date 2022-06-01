d69ccd3486c9254df54a2d4d2d7e7d937b9a6f0d
mishkamag <m93.maglaperidze@gmail.com>
2022-06-01 22:00:28 +0400

    Meme Generator

---
+++ script.js
@@ -0,0 +1,56 @@
+const imageFileInput = document.querySelector("#imageFileInput");
+const topTextInput = document.querySelector("#topTextInput");
+const bottomTextInput = document.querySelector("#bottomTextInput");
+const canvas = document.querySelector("#meme");
+
+let image;
+
+imageFileInput.addEventListener("change", () => {
+  const imageDataUrl = URL.createObjectURL(imageFileInput.files[0]);
+
+  image = new Image();
+  image.src = imageDataUrl;
+
+  image.addEventListener(
+    "load",
+    () => {
+      upadateMemeCanvas(canvas, image, topTextInput.value, bottomTextInput);
+    },
+    { once: true }
+  );
+});
+
+topTextInput.addEventListener("change", () => {
+  upadateMemeCanvas(canvas, image, topTextInput.value, bottomTextInput.value);
+});
+
+bottomTextInput.addEventListener("change", () => {
+  upadateMemeCanvas(canvas, image, topTextInput.value, bottomTextInput.value);
+});
+
+function upadateMemeCanvas(canvas, image, topText, bottomText) {
+  const ctx = canvas.getContext("2d");
+  const width = image.width;
+  const height = image.height;
+  const fontSize = Math.floor(width / 10);
+  const yOffset = height / 25;
+
+  canvas.width = width;
+  canvas.height = height;
+  ctx.drawImage(image, 0, 0);
+
+  ctx.strokeStyle = "black";
+  ctx.lineWidth = Math.floor(fontSize / 4);
+  ctx.fillStyle = "white";
+  ctx.textAlign = "center";
+  ctx.lineJoin = "round";
+  ctx.font = `${fontSize}px sans-sarif`;
+
+  ctx.textBaseline = "top";
+  ctx.strokeText(topText, width / 2, yOffset);
+  ctx.fillText(topText, width / 2, yOffset);
+
+  ctx.textBaseline = "bottom";
+  ctx.strokeText(bottomText, width / 2, height - yOffset);
+  ctx.fillText(bottomText, width / 2, height - yOffset);
+}
